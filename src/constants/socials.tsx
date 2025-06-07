53ea5559023401864a1ffb99329c5cf31028297b
Srivatsa Nayak <nayaksrivatsa15@gmail.com>
2025-06-07 17:57:45 +0530

    make footer fixed

--- src/constants/socials.tsx
+++ src/constants/socials.tsx
@@ -5,11 +5,11 @@ import {
 } from "@tabler/icons-react";
 
 export const socials = [
-  {
-    href: "https://twitter.com/mannupaaji",
-    label: "Instagram",
-    icon: IconBrandInstagram,
-  },
+  // {
+  //   href: "https://twitter.com/mannupaaji",
+  //   label: "Instagram",
+  //   icon: IconBrandInstagram,
+  // },
   {
     href: "https://www.linkedin.com/in/srivatsa-nayak-179409236/",
     label: "LinkedIn",
